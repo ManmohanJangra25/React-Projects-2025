782948826f2a203943f51e225ce31ad1c35dfcd0
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2025-02-05 02:13:31 +0530

    Routes Addes

---
+++ src/components/routes/routes.js
@@ -0,0 +1,7 @@
+import App from "../../App";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/about", element: <p>About Us</p> },
+]);
