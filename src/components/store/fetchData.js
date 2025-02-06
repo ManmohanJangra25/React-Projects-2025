b76a770743300cbd3c09ed5335f1dfea84dcf0d4
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2025-02-06 23:54:20 +0530

    Updates

--- src/components/store/fetchData.js
+++ src/components/store/fetchData.js
@@ -1,8 +1,5 @@
 const myHeaders = new Headers();
-myHeaders.append(
-  "Authorization",
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDg2MjkwN2EyYmE4NmMxZjc0ZTZiMTdhZTcwNjg2OSIsIm5iZiI6MTczODcwMzA1Mi44ODksInN1YiI6IjY3YTI4MGNjYThiNmZjZTU1ZDhhNjFkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tsCQL3FmWiK_iPosbS7xdVc3k8X8k2Oj2czRiNgQdV4"
-);
+myHeaders.append("Authorization", "Bearer " + process.env.REACT_APP_API_KEY);
 
 const requestOptions = {
   method: "GET",
